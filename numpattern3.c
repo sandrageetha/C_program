2acc5554d4056228f6a52a0235e3d3118de48e28
sandrageetha <sandrageetha02@gmail.com>
2024-11-19 15:54:45 +0530

    test1

--- numpattern3.c
+++ numpattern3.c
@@ -2,12 +2,13 @@
 #include<stdlib.h>
 
 int main(void){
-    int num,i,j;
+    int num,i,j,c=1;
     printf("Enter the size of array :");
     scanf("%d",&num);
     for( i=1;i<=num;i++){
-        for(j=0;j>num;j++){
-            printf("%d\t", i);
+        for(j=0;j<i;j++){
+            printf("%d\t", c);
+            c++;
         }
         printf("\n");
     }
